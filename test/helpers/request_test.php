6bc56446efa58c7a4e6e82d1c856a3e3b9f3326b
claudiopro <claudio.procida@gmail.com>
2015-11-27 13:53:21 +0000

    Adds tests for Request helper

---
+++ test/helpers/request_test.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../helpers/request.php');
+
+class MockRequest extends Request
+{
+  public static function purge_querystring_spy()
+  {
+    return self::purge_querystring();
+  }
+}
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+  public function test_construct()
+  {
+    $request = new Request();
+    $this->assertNotNull($request);
+  }
+
+  public function test_get_parameter()
+  {
+    $request = new Request();
+    $_REQUEST['foo'] = 'bar';
+    $this->assertEquals('bar', $request->get_parameter('foo'));
+  }
+
+  public function test_is_get()
+  {
+    $request = new Request();
+    $_SERVER['REQUEST_METHOD'] = 'GET';
+    $this->assertTrue($request->is_get());
+  }
+
+  public function test_is_post()
+  {
+    $request = new Request();
+    $_SERVER['REQUEST_METHOD'] = 'POST';
+    $this->assertTrue($request->is_post());
+  }
+
+  public function test_is_put()
+  {
+    $request = new Request();
+    $_SERVER['REQUEST_METHOD'] = 'PUT';
+    $this->assertTrue($request->is_put());
+  }
+
+  public function test_is_delete()
+  {
+    $request = new Request();
+    $_SERVER['REQUEST_METHOD'] = 'DELETE';
+    $this->assertTrue($request->is_delete());
+  }
+
+  public function test_purge_querystring()
+  {
+    define(QS1, 'foo=bar&baz=1');
+
+    $_SERVER['QUERY_STRING'] = QS1;
+    $this->assertEquals(QS1, MockRequest::purge_querystring_spy());
+  }
+}
+?>
