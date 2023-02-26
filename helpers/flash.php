8b7582ac95edcc35b0fa9e10532e5ff2fb93a7c1
claudiopro <claudio.procida@gmail.com>
2023-02-26 09:12:56 +0100

    Contributes Flash helper to core

---
+++ helpers/flash.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ *                                   _ __
+ *   ___  ____ ___  ___  _________ _(_) /____
+ *  / _ \/ __ `__ \/ _ \/ ___/ __ `/ / / ___/
+ * /  __/ / / / / /  __/ /  / /_/ / / (__  )
+ * \___/_/ /_/ /_/\___/_/   \__,_/_/_/____/
+ *
+ * (c) Claudio Procida 2008-2023
+ *
+ * @format
+ */
+
+class Flash
+{
+    private static $instance = null;
+
+    private function __construct()
+    {
+        $this->message = $_SESSION['flash']['message'];
+        $this->type = $_SESSION['flash']['type'];
+    }
+
+    public function get_css_class()
+    {
+        $cls = array('flash', 'msg');
+        switch ($this->type) {
+            case 'info':
+                $cls[] = 'info';
+                break;
+            case 'warning':
+                $cls[] = 'warning';
+                break;
+            case 'success':
+                $cls[] = 'success';
+                break;
+            case 'error':
+                $cls[] = 'error';
+                break;
+        }
+        return implode(' ', $cls);
+    }
+
+    public static function has()
+    {
+        return isset($_SESSION['flash']);
+    }
+
+    public static function get()
+    {
+        if (isset($_SESSION['flash'])) {
+            if (!get_called_class()::$instance) {
+                $classname = get_called_class();
+                get_called_class()::$instance = new $classname();
+            }
+            return get_called_class()::$instance;
+        }
+        return null;
+    }
+
+    public static function destroy()
+    {
+        get_called_class()::$instance = null;
+        unset($_SESSION['flash']);
+    }
+}
---
+++ public/Helpers/Flash.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *                                   _ __
+ *   ___  ____ ___  ___  _________ _(_) /____
+ *  / _ \/ __ `__ \/ _ \/ ___/ __ `/ / / ___/
+ * /  __/ / / / / /  __/ /  / /_/ / / (__  )
+ * \___/_/ /_/ /_/\___/_/   \__,_/_/_/____/
+ *
+ * (c) Claudio Procida 2008-2023
+ *
+ * @format
+ */
+
+namespace Emeraldion\EmeRails\Helpers;
+
+require_once __DIR__ . '/../../helpers/flash.php';
+
+class Flash extends \Flash
+{
+}
+
+?>
