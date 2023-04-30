a14cb01039c91e90df02c1add2e71301c609190b
claudiopro <claudio.procida@gmail.com>
2023-05-01 01:14:19 +0200

    Fixes ErrorDocument directives

--- views/error/_404.php
+++ views/error/_404.php
@@ -14,5 +14,7 @@
 $this->set_title('404 - Not Found'); ?>
 <h1>404 - Not Found</h1>
 <p>
-	Uh-oh! The requested resource does not exist (yet?) on this server.
+	Uh-oh! The requested resource <strong><?php print $_SERVER[
+     'REDIRECT_URL'
+ ]; ?></strong> does not exist (yet?) on this server.
 </p>
