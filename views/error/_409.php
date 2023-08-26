8c21b89e4eb550e5a2f3ad817a421238522af48c
claudiopro <claudio.procida@gmail.com>
2023-08-26 21:27:21 +0200

    Adds view for 409 Conflict error

---
+++ views/error/_409.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *                                   _ __
+ *   ___  ____ ___  ___  _________ _(_) /____
+ *  / _ \/ __ `__ \/ _ \/ ___/ __ `/ / / ___/
+ * /  __/ / / / / /  __/ /  / /_/ / / (__  )
+ * \___/_/ /_/ /_/\___/_/   \__,_/_/_/____/
+ *
+ * (c) Claudio Procida 2008-2023
+ *
+ * @format
+ */
+
+$this->set_title('409 - Conflict'); ?>
+<h1>409 - Conflict</h1>
+<p>
+	The request conflicts with the current state of the target resource.
+</p>
