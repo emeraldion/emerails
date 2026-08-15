fb4dda92677a889cf6adebde5973c94b7f5f96c7
claudiopro <claudio.procida@gmail.com>
2026-08-15 17:21:50 +0200

    Terminal startup script

---
+++ scripts/terminal_startup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#                                   _ __
+#   ___  ____ ___  ___  _________ _(_) /____
+#  / _ \/ __ `__ \/ _ \/ ___/ __ `/ / / ___/
+# /  __/ / / / / /  __/ /  / /_/ / / (__  )
+# \___/_/ /_/ /_/\___/_/   \__,_/_/_/____/
+#
+# (c) Claudio Procida 2008-2026
+#
+
+# Import common utils
+source $(dirname "$0")/common.sh
+# Display project logo
+logo
+# Setup node
+nvm use
